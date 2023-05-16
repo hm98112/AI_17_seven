543475413d6d73e78d8fb13e435707834d9d4018
hamin ryu <hm98112@gmail.com>
2023-05-16 21:16:58 +0900

    README 1일차 오후 회의 내역 업데이트

--- attraction.py
+++ attraction.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+import subprocess
 
 driver = webdriver.Chrome("C:/Users/hm981/Downloads/chromedriver_win32/chromedriver.exe")
 
@@ -22,13 +23,24 @@ for i in range(1, 101):
     driver.get("https://kr.trip.com/travel-guide/attraction/paris-308/tourist-attractions/"+str(i)+".html")
     driver.implicitly_wait(time_to_wait=10)
     time.sleep(1)
-    texts = driver.find_elements(By.CLASS_NAME, "poi-name margin-bottom-gap")
+    texts = driver.find_elements(By.CLASS_NAME, "card-right")
 
     for text in texts:
         print(text.text)
     
     
-
-
-
 driver.quit()
+
+
+
+
+# 현재 환경에 설치된 패키지 목록을 얻습니다.
+packages = subprocess.check_output(['pip', 'freeze']).decode('utf-8').split('\n')
+
+# requirements.txt 파일을 생성하고 패키지 목록을 저장합니다.
+with open('requirements.txt', 'w') as file:
+    for package in packages:
+        file.write(package + '\n')
+
+
+
