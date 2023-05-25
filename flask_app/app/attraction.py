2b345bbb62a02eca6416441612a1ec3006ef65ec
asyam9 <asyam6699@gmail.com>
2023-05-25 11:48:29 +0900

    bug fix attraction-check price button

--- flask_app/app/attraction.py
+++ flask_app/app/attraction.py
@@ -19,13 +19,13 @@ conn = psycopg2.connect(
 #"/attraction" url 접근 함수
 @bp.route('/attraction', methods=['POST', 'GET'])
 def index():
-    
-    cursor = conn.cursor()
 
     #전역 변수에 이전 페이지(호텔)에서 넘어온 가격 저장
     current_app.config['hotel_price'] = request.form.get('hotel-price')
     current_app.config['hotelTotalPrice'] = request.form.get('hotelTotalPrice')
     current_app.config['count_people'] = request.form.get('count_people')
+
+    cursor = conn.cursor()
     #카테고리 리스트
     cursor.execute("SELECT DISTINCT category FROM attraction")
     categories = cursor.fetchall()
@@ -44,7 +44,10 @@ def index():
 #"/attraction/price-selection" url 접근 함수 |어트랙션 가격 확인 기능 동작|
 @bp.route('/attraction/price-selection', methods=['POST','GET'])
 def price_selection():
-
+    
+    # POST 메소드로 전송된 데이터를 request 객체를 통해 액세스 가능
+    count_people = current_app.config['count_people']
+    
     #필터에서 선택한 특성들 불러오기
     selected_category = request.form.get('attraction-category')
     att_order = request.form.get('order')
@@ -69,7 +72,7 @@ def price_selection():
 
     cursor.close()
 
-    return render_template('attraction.html', categories=categories, results=sorted_results, att_order=att_order,selected_category=selected_category)
+    return render_template('attraction.html', categories=categories,count_people=count_people, results=sorted_results, att_order=att_order,selected_category=selected_category)
 
 
 
